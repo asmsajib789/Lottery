88290fe7f483c0af150feab1620f63b0b76713b7
Sajib <107872992+asmsajib789@users.noreply.github.com>
2025-07-24 04:04:12 +0600

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 const API_KEY = "$2a$10$8yMSQ6oJufNY23oj/hGWhOYCNKMB2S22m.mkuj21pCtzDXuIVEdVy";
 const BIN_ID = "68814a5ef7e7a370d1ecd43f";
 const BIN_URL = `https://api.jsonbin.io/v3/b/${BIN_ID}`;
+const ADMIN_PASSWORD = "Oxy1122";
